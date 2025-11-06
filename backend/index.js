d2de45ec061aa5675a3bbd1e256166b3bd4230cc
Pratchaya-jamp <alone9364@gmail.com>
2025-11-06 22:57:45 +0700

    add CORS & Error exception

--- backend/index.js
+++ backend/index.js
@@ -9,3 +9,23 @@ admin.initializeApp({
 })
 const db = admin.firestore()
 const urlsCollection = db.collection('urls')
+
+const PORT = process.env.PORT
+const BASE_URL = process.env.BASE_URL
+const FRONTEND_URL = process.env.FRONTEND_URL
+
+function sendJson(res, statusCode, data) {
+  res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Access-Control-Allow-Origin', FRONTEND_URL)
+  res.writeHead(statusCode)
+  res.end(JSON.stringify(data))
+}
+function sendRedirect(res, longUrl) {
+  res.setHeader('Location', longUrl)
+  res.setHeader('Access-Control-Allow-Origin', FRONTEND_URL)
+  res.writeHead(301)
+  res.end()
+}
+function sendError(res, statusCode, message) {
+  sendJson(res, statusCode, { error: message })
+}
