ebd6856a5c313cb49ba87a5c16f35bd243901a17
Pratchaya-jamp <alone9364@gmail.com>
2025-11-06 23:13:38 +0700

    set react template layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styled from '@emotion/styled'
+import { Global, keyframes } from "@emotion/react";
+import { FaLink, FaSpinner, FaCheckCircle, FaExclamationCircle, FaCopy, FaHistory } from 'react-icons/fa'
+
+const API_URL = process.env.REACT_APP_BACKEND
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppLayOut>
+      <AppHeader>
+        <MainContent>
+          
+        </MainContent>
+      </AppHeader>
+    </AppLayOut>
+  )
 }
 
 export default App;
