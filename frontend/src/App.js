2161e0b28a6de7701109edcd8824ef727004b97b
Pratchaya-jamp <alone9364@gmail.com>
2025-11-08 10:55:13 +0700

    add validate format url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -331,6 +331,19 @@ const AppFooter = () => (
   </Footer>
 );
 
+const isValidUrl = (string) => {
+  const pattern = new RegExp(
+    '^(https?:\\/\\/)?' +
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+    '((\\d{1,3}\\.){3}\\d{1,3}))' +
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+    '(\\?[;&a-z\\d%_.~+=-]*)?' +
+    '(\\#[-a-z\\d_]*)?$',
+    'i'
+  )
+  return !!pattern.test(string)
+}
+
 function App() {
   return (
     <AppLayout>
